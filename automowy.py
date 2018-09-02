b9765bff740fb2abfea95da0e895dcd67981f4cb
Thomas Monjalon <thomas@monjalon.net>
2018-09-03 00:18:36 +0200

    add basic functions for API requests
    The API is split in 3 parts:
    	- query (GET)
    	- control (POST)
    	- set (PUT)
    
    The settings can be read and updated with query and set.
    The control function is used for start/stop requests.
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

--- automowy.py
+++ automowy.py
@@ -3,13 +3,19 @@
 import requests
 
 
+class AutomowyError(Exception):
+    pass
+
+
 class AutomowySession:
     AUTH_URL_BASE = 'https://iam-api.dss.husqvarnagroup.net/api/v3/token/'
+    URL_BASE = 'https://amc-api.dss.husqvarnagroup.net/app/v1/'
 
     def __init__(self):
         self.session = requests.Session()
         self.user = None
         self.token = None
+        self.mowers = None
 
     def login(self, user, password):
         response = self.session.post(
@@ -35,3 +41,71 @@ class AutomowySession:
         response.raise_for_status()
         self.token = None
         del (self.session.headers['Authorization'])
+
+    def list_mowers(self):
+        response = self.session.get(
+            self.URL_BASE + 'mowers'
+        )
+        response.raise_for_status()
+        json = response.json()
+        self.mowers = [
+            {'id': mower['id'], 'name': mower['name']}
+            for mower in json
+        ]
+        return json
+
+    def find_mower(self, match=None):
+        return Automowy(self, match)
+
+
+class Automowy:
+    def __init__(self, session, match=None):
+        self.session = session
+        if not session.mowers:
+            session.list_mowers()
+        if not len(self.session.mowers):
+            raise AutomowyError('No mower for %s' % session.user)
+        if match:
+            for mower in session.mowers + [None]:
+                if mower is None:
+                    raise AutomowyError('No mower matching %s' % match)
+                if mower['id'] == match or mower['name'] == match:
+                    break
+        else:
+            mower = session.mowers[0]
+        self.name = mower['name']
+        self.id = mower['id']
+        self.url = session.URL_BASE + 'mowers/%s/' % self.id
+
+    def query(self, command):
+        """
+        :param command: can be 'status', 'geofence' or 'settings'
+        """
+        response = self.session.session.get(
+            self.url + command
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def control(self, command, param=None):
+        """
+        :param command: can be 'start', 'start/override/period',
+            'pause', 'park', 'park/duration/timer'
+        :param param: must be {'period':minutes} for 'start/override/period'
+        """
+        response = self.session.session.post(
+            self.url + 'control/%s' % command,
+            json=param
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def set(self, key, value):
+        """
+        :param key: examples: 'cuttingHeight', 'ecoMode'
+        """
+        response = self.session.session.put(
+            self.url + 'settings',
+            json={'settings': {key: value}}
+        )
+        response.raise_for_status()
--- test.py
+++ test.py
@@ -4,6 +4,15 @@
 import automowy
 
 session = automowy.AutomowySession()
-session.login('john@doe.com', 'secret')
+mower = session.login('john@doe.com', 'secret').find_mower()
+
+print(mower.query('status'))
+
+mower.control('start/override/period', {'period': 300})
+mower.control('start')
+mower.control('pause')
+mower.control('park')
+mower.control('park/duration/timer')
+mower.set('cuttingHeight', 4)
 
 session.logout()
